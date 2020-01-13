ef24d92d33388b03bb79fa9d73204bc401d0d525
Mikhail <levinmk23@gmail.com>
2020-01-13 22:08:00 +0300

    classwork 4

--- task4/src/main/java/TaskRunner.java
+++ task4/src/main/java/TaskRunner.java
@@ -24,6 +24,7 @@ public class TaskRunner implements Runnable {
         this.mutex = mutex;
         message = msg; //каждый поток может писать в список только
         //его сообщение!!!
+        //System.out.println(message);
         this.cnt = cnt;
         inc = 0;
     }
@@ -31,9 +32,20 @@ public class TaskRunner implements Runnable {
     @Override
     public void run() {
         // TODO: 26.12.2019
-        while (true) {
-            list.add(message);
+        while (list.size() < 60) {
+            //synchronized (mutex) {
+                if ((iter % 30) == cnt) {
+                    list.add(message);
+                    iter++;
+//                    try {
+//                        Thread.sleep(100);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+                }
+            //}
         }
+        System.out.println(list);
     }
 
 }
